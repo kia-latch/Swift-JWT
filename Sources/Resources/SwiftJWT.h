eef38f4a74e2324f177110a03241ee477ea1028f
Kiarash Asar <kiarash.asar@latch.com>
2019-12-01 21:27:03 -0800

    Added project with a tvOS framework target.

---
+++ Sources/Resources/SwiftJWT.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJWT.h
+//  SwiftJWT
+//
+//  Created by Kiarash Asar on 12/1/19.
+//  Copyright © 2019 Latchable, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftJWT.
+FOUNDATION_EXPORT double SwiftJWTVersionNumber;
+
+//! Project version string for SwiftJWT.
+FOUNDATION_EXPORT const unsigned char SwiftJWTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJWT/PublicHeader.h>
+
+
